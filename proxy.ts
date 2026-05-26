d6b620a24bf509e7162b676f415ed05a59928e88
Ylya <ylyamartchenko@gmail.com>
2026-05-27 01:59:03 +0200

    ylya bot metrics analysis

--- proxy.ts
+++ proxy.ts
@@ -3,39 +3,20 @@ import { NextRequest, NextResponse } from "next/server";
 export async function proxy(req: NextRequest) {
   const response = NextResponse.next();
 
-  console.log("full req:", req);
-  console.log("headers", req.headers);
-
   const existingIp = req.cookies.get("user_ip")?.value;
-  const existingCountry = req.cookies.get("user_country")?.value;
-  if (!existingIp || !existingCountry) {
+  if (!existingIp) {
     const ip =
       req.headers.get("x-real-ip") ||
       req.headers.get("x-forwarded-for")?.split(",")[0] ||
       "unknown";
-    const country = req.headers.get("x-vercel-ip-country") || "unknown";
 
-    if (!existingIp) {
-      response.cookies.set("user_ip", ip, {
-        path: "/",
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
-        maxAge: 60 * 60 * 24 * 30,
-      });
-    }
-
-    // TODO: add metrics for ylya bot
-    // FIXME: wrong country retreived
-    if (!existingCountry) {
-      response.cookies.set("user_country", country, {
-        path: "/",
-        httpOnly: false,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
-        maxAge: 60 * 60 * 24 * 30,
-      });
-    }
+    response.cookies.set("user_ip", ip, {
+      path: "/",
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+      maxAge: 60 * 60 * 24 * 30,
+    });
   }
 
   return response;
